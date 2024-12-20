5febd5147c455f4a5d6ce21f24747a1591200c64
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-12-20 12:20:49 -0500

    Style the home page
    Center the tag line and make the main color.
    Center the Philippine flag circular icon.
    Place restaurant info in brown box.
    Space out hour information using grid, center the hours
    section label and give an underline.

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
                 use: ["style-loader", "css-loader"],
             },
             {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: "asset/resource",
             },
