3bf18ad45c15ff1912890224fc14d653de1c7c41
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-12-20 00:55:47 -0500

    Style page header and buttons
    Style additions
    General - Set main, bg, and accent colors
    Header - Bg image, bg color, centering, shadows
    Buttons - Sizing. border radius, colors
    
    Style button of "active" page using the .open class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { HomePageBuilder } from "./home_builder";
 import { MenuPageBuilder } from "./menu_builder";
 import { AboutPageBuilder } from "./about_builder";
+import "./styles.css";
 
 console.log("Script is running");
 HomePageBuilder();
