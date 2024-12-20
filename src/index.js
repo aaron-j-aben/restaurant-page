584dbe7c6a54670bfcdab6c19c7542a162a278e8
aaron-j-aben <aaron.jm.aben@gmail.com>
2024-12-20 14:05:53 -0500

    Create IIFE that assigns listeners to nav buttons to handle tab switching

--- src/index.js
+++ src/index.js
@@ -9,3 +9,28 @@ HomePageBuilder();
 /*
     Nav button listeners
 */
+const setTabbing = (function() {
+    const content = document.querySelector("#content");
+    const homeBtn = document.querySelector("nav .home");
+    const menuBtn = document.querySelector("nav .menu");
+    const aboutBtn = document.querySelector("nav .about");
+
+    for (const prts of [[homeBtn, HomePageBuilder], [menuBtn, MenuPageBuilder], [aboutBtn, AboutPageBuilder]]) {
+        prts[0].addEventListener("click", (function(e) {
+            // clear tab
+            while (content.firstChild) {
+                content.removeChild(content.firstChild);
+            }
+
+            // print out new tab
+            prts[1]();
+
+            // change color of btn of active tab
+            for (const b of [homeBtn, menuBtn, aboutBtn]) {
+                b.classList.remove("open");
+            }
+            
+            e.target.classList.add("open");
+        }));
+    }
+})();
